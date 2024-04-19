660f70334d3ef2ae311d6fded150a5d9f84a646c
Gauch Kirill <kgauch@mail.ru>
2024-04-19 19:21:55 +0300

    [model] Created ferst version of engine

--- parameters.m
+++ parameters.m
@@ -1,10 +1,10 @@
-duty_cycle = 50;
-PWM_freq = 1;
+duty_cycle = 99;
+PWM_freq = 2000;
+PWM_period = 1/PWM_freq;
 pairs = 20;
 nominal_voltage = 48;
 nominal_torque = 0.75;
 nominal_current = 3.5;
-r = 1.930;
 K_t = 0.218;
 rotation_speed = 1820 / 60;
 D = 0.068;
