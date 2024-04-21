530f718d7e042dc7f16dea8ee3510a847e0a90a2
Gauch Kirill <kgauch@mail.ru>
2024-04-21 13:13:27 +0300

    [general] Distributed the data - Create new '.m' files with parameters for partilular models - Moved the auxiliary modules to the 'include' folder

---
+++ angular_position_sensor_param.m
@@ -0,0 +1,3 @@
+freq_update = 71400;
+period_update = 1/freq_update;
+bit_depth = 18;
--- parameters.m
+++ parameters.m
@@ -1,16 +1,5 @@
 duty_cycle = 99.9999;
 PWM_freq = 20000;
 PWM_period = 1/PWM_freq;
-freq_update = 71400;
-period_update = 1/freq_update;
-pairs = 20;
-nominal_voltage = 48;
-nominal_torque = 0.75;
-nominal_current = 3.5;
-K_t = 0.218;
-rotation_speed = 1820 / 60;
-D = 0.068;
-d = 0.042;
-m = 0.225;
-I = m * (d^2 + D^2) / 8;
-bit_depth = 18;
+run('engine_model_param.m');
+run('angular_position_sensor_param.m');
